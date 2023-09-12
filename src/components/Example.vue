4e3d161773cf6c821898ca0c9394995d0105ea53
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2023-09-12 23:52:28 +0530

    Second commit

---
+++ src/components/Example.vue
@@ -0,0 +1,12 @@
+<template>
+    <h3>{{ example.text }} </h3>
+</template>
+
+<script>
+    export default{
+        name: 'Example',
+        props:{
+            example: Object,
+        },
+    }
+</script>
